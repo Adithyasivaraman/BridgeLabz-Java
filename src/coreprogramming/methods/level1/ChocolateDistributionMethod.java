74c01784b3b705adb75816e5782074adeb0ac935
adithya <adithyasivaraman5@gmail.com>
2026-01-19 12:58:52 +0530

    [Adithya] : Added : Methods

---
+++ src/coreprogramming/methods/level1/ChocolateDistributionMethod.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ChocolateDistributionMethod {
+    public static int[] findRemainderAndQuotient(int chocolates, int children) {
+        return new int[]{chocolates / children, chocolates % children};
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int chocolates = sc.nextInt();
+        int children = sc.nextInt();
+
+        int[] result = findRemainderAndQuotient(chocolates, children);
+        System.out.println("Each child gets " + result[0] + " chocolates, Remaining = " + result[1]);
+    }
+}
